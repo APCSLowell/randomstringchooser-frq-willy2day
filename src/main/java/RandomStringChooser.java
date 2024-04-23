5d49756a8e6d040130d2338c26b5b5eb9a8f279e
willy2day <143228693+willy2day@users.noreply.github.com>
2024-04-23 11:09:32 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -2,7 +2,18 @@ import java.util.*;
 public class RandomStringChooser
 {
   /* to be implemented in part (a) */
-  
+  private String [] wordArray;
+  public RandomStringChooser (String [] n){
+    wordArray=n; 
+    
+  }
+  public string getNext(){
+    String w="";
+    for (int i =0; i <wordArray.length;i++){
+      w+=wordArray[(int)* Math.random()*wordArray.length;
+    }
+    return w; 
+  }
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
 }
